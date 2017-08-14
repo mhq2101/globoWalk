6a4e5d5a481da46a2fe1437b865be9571372c0f3
Dan Meisel <dmeis654@gmail.com>
2017-08-14 12:20:41 -0400

    changed secret

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Route, browserHistory, IndexRedirect, Switch } from 'react-router';
+import Audio from './Audio'
 import Home from './Home'
+import {fetchAudio} from '../redux/reducers/audioStream.jsx';
+import store from '../store.jsx';
 
 // import App from './components/App';
 // import Home from './components/Login/Home';
@@ -13,12 +16,18 @@ import Home from './Home'
 
 export default class Root extends React.Component {
 
+    componentDidMount() {
+      store.dispatch(fetchAudio())
+      
+    }
+
     render() {
         return (
             <div>
                 <div>
                     <Switch>
                         <Route exact path='/' component={Home} />
+                        <Route exact path='/audio' component={Audio} />
                     </Switch>
                 </div>
 
--- app/redux/reducers/index.jsx
+++ app/redux/reducers/index.jsx
@@ -1,11 +1,19 @@
 import { combineReducers } from 'redux';
 import dummy from './dummy.jsx';
 import auth from './auth';
+import audioStream from './audioStream';
+import audioContext from './audioContext';
+import webRtc from './webRtc';
+
 
 
 const rootReducer = combineReducers({
     dummy: dummy,
-    auth: auth
+    auth: auth,
+    audioStream: audioStream,
+    audioCtx: audioContext,
+    webRtc: webRtc
+
 });
 
 
