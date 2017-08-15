5c876011e3d4314f55756782391490ce87233126
Mikhail Qader <mhq2101@columbia.edu>
2017-08-15 16:45:14 -0400

    database Changes

--- db/models/index.js
+++ db/models/index.js
@@ -6,7 +6,9 @@ const Session = require('./session');
 Session.belongsTo(User);
 User.hasOne(Session);
 
-User.belongsToMany(Chatroom, {through: 'User_Chatrooms'})
-Chatroom.belongsToMany(User, {through: 'Chatroom_Users'})
+// User.belongsToMany(Chatroom, {through: 'User_Chatrooms'})
+// Chatroom.belongsToMany(User, {through: 'Chatroom_Users'})
+
+Chatroom.hasMany(User)
 
 module.exports = {Chatroom, User, Session};
